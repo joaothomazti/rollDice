f1a33a9eadd11b79c2b5e774e42d224bdd6e0120
joaothomazti <joaothomazti@hotmail.com>
2022-08-25 15:38:36 -0300

    Adicionando a imagem de um dado

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.rolldice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rolldice.R.drawable.*
 import com.example.rolldice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,12 +19,20 @@ class MainActivity : AppCompatActivity() {
 
         val button = binding.roll
         button.setOnClickListener { rollDice() }
+        rollDice()
     }
 
     private fun rollDice() {
         val dice = Dice(numSides = 6)
         val diceRoll = dice.roll()
-        val resultDiceRoll = binding.diceRoll
-        resultDiceRoll.text = diceRoll.toString()
+        val diceImg = binding.imgDice
+        when(diceRoll){
+            1 -> diceImg.setImageResource(dice_1)
+            2 -> diceImg.setImageResource(dice_2)
+            3 -> diceImg.setImageResource(dice_3)
+            4 -> diceImg.setImageResource(dice_4)
+            5 -> diceImg.setImageResource(dice_5)
+            else -> diceImg.setImageResource(dice_6)
+        }
     }
 }
