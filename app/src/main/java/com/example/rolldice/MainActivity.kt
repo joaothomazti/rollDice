44910546a3572f2811c184708de90c1dbd2d8703
joaothomazti <joaothomazti@hotmail.com>
2022-08-25 13:56:54 -0300

    Aplicando processo de binding no projeto

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -2,23 +2,28 @@ package com.example.rolldice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import com.example.rolldice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
 
-        val button = findViewById<Button>(R.id.roll)
+        val button = binding.roll
         button.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
         val dice = Dice(numSides = 6)
         val diceRoll = dice.roll()
-        val resultDiceRoll = findViewById<TextView>(R.id.dice_roll)
+        val resultDiceRoll = binding.diceRoll
         resultDiceRoll.text = diceRoll.toString()
     }
 }
